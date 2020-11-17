2c388c98276386cc4f88f0d488268a1f6132765b
akira <linkomizin@gmail.com>
2020-11-17 13:18:29 +0300

    Update qt5.py

--- qt5.py
+++ qt5.py
@@ -9,6 +9,7 @@ data_now = datetime.now().strftime('%Y, %m, %d')
 class Ui_Form(QtWidgets.QWidget):
     def __init__(self, parent=None):
         QtWidgets.QWidget.__init__(self, parent)
+
         self.hbox = QtWidgets.QHBoxLayout()
         self.setLayout(self.hbox)
         self.vbox = QtWidgets.QVBoxLayout()
@@ -16,12 +17,17 @@ class Ui_Form(QtWidgets.QWidget):
         self.hbox.addLayout(self.vbox)
         self.setLayout(self.vbox)
 
+        # блок выбора машины
         self.vbox_car = QtWidgets.QVBoxLayout()
         self.hbox.addLayout(self.vbox_car)
 
+        # блок вывода
+        self.vbox_text = QtWidgets.QVBoxLayout()
+        self.hbox.addLayout(self.vbox_text)
+
+
         self.dateEdit = QtWidgets.QDateEdit()
         self.dateEdit.setGeometry(QtCore.QRect(10, 10, 110, 24))
-        # self.dateEdit.setDateTime(QtCore.QDateTime(QtCore.QDate(2020, 11, 21), QtCore.QTime(0, 0, 0)))
         self.dateEdit.setDate(QtCore.QDate.currentDate())
         self.dateEdit.setCalendarPopup(True)
         self.vbox.addWidget(self.dateEdit)
@@ -30,17 +36,32 @@ class Ui_Form(QtWidgets.QWidget):
         self.pushButton.setGeometry(QtCore.QRect(0, 60, 113, 32))
         self.vbox.addWidget(self.pushButton)
 
-        self.radioButton1 = QtWidgets.QRadioButton('400')
-        self.radioButton1.setGeometry(QtCore.QRect(130, 10, 100, 20))
-        self.vbox_car.addWidget(self.radioButton1)
+        self.radioButton = QtWidgets.QRadioButton('400')
+        self.radioButton.setGeometry(QtCore.QRect(130, 10, 100, 20))
+        self.vbox_car.addWidget(self.radioButton)
+        self.radioButton.value = 'К400ХН76'
+        self.radioButton.toggled.connect(self.on_clicked)
 
-        self.radioButton2 = QtWidgets.QRadioButton('509')
-        self.radioButton2.setGeometry(QtCore.QRect(130, 10, 100, 20))
-        self.vbox_car.addWidget(self.radioButton2)
+        self.radioButton = QtWidgets.QRadioButton('509')
+        self.radioButton.setGeometry(QtCore.QRect(130, 10, 100, 20))
+        self.vbox_car.addWidget(self.radioButton)
+        self.radioButton.value = 'А509КО76'
+        self.radioButton.toggled.connect(self.on_clicked)
 
-        self.radioButton3 = QtWidgets.QRadioButton('992')
-        self.radioButton3.setGeometry(QtCore.QRect(130, 10, 100, 20))
-        self.vbox_car.addWidget(self.radioButton3)
+        self.radioButton = QtWidgets.QRadioButton('992')
+        self.radioButton.setGeometry(QtCore.QRect(130, 10, 100, 20))
+        self.vbox_car.addWidget(self.radioButton)
+        self.radioButton.value = 'В992РО76'
+        self.radioButton.toggled.connect(self.on_clicked)
+
+        self.label = QtWidgets.QLabel()
+        self.vbox_text.addWidget(self.label)
+
+
+    def on_clicked(self):
+        radioButton = self.sender()
+        if radioButton.isChecked():
+            self.label.setText(radioButton.value)
 
 
 if __name__ == '__main__':
