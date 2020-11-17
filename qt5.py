e9323b5d3735ecd4c9d3aeec7e95760ffa9787ad
akira <linkomizin@gmail.com>
2020-11-17 11:15:06 +0300

    Update qt5.py

--- qt5.py
+++ qt5.py
@@ -1,5 +1,9 @@
 import sys
 from PyQt5 import QtCore, QtWidgets
+from datetime import datetime
+
+data_now = datetime.now().strftime('%Y, %m, %d')
+
 
 
 class Ui_Form(QtWidgets.QWidget):
@@ -17,7 +21,8 @@ class Ui_Form(QtWidgets.QWidget):
 
         self.dateEdit = QtWidgets.QDateEdit()
         self.dateEdit.setGeometry(QtCore.QRect(10, 10, 110, 24))
-        self.dateEdit.setDateTime(QtCore.QDateTime(QtCore.QDate(2020, 1, 1), QtCore.QTime(0, 0, 0)))
+        # self.dateEdit.setDateTime(QtCore.QDateTime(QtCore.QDate(2020, 11, 21), QtCore.QTime(0, 0, 0)))
+        self.dateEdit.setDate(QtCore.QDate.currentDate())
         self.dateEdit.setCalendarPopup(True)
         self.vbox.addWidget(self.dateEdit)
 
@@ -25,17 +30,18 @@ class Ui_Form(QtWidgets.QWidget):
         self.pushButton.setGeometry(QtCore.QRect(0, 60, 113, 32))
         self.vbox.addWidget(self.pushButton)
 
-        self.radioButton = QtWidgets.QRadioButton('400')
-        self.radioButton.setGeometry(QtCore.QRect(130, 10, 100, 20))
-        self.vbox_car.addWidget(self.radioButton)
+        self.radioButton1 = QtWidgets.QRadioButton('400')
+        self.radioButton1.setGeometry(QtCore.QRect(130, 10, 100, 20))
+        self.vbox_car.addWidget(self.radioButton1)
 
-        self.radioButton = QtWidgets.QRadioButton('509')
-        self.radioButton.setGeometry(QtCore.QRect(130, 10, 100, 20))
-        self.vbox_car.addWidget(self.radioButton)
+        self.radioButton2 = QtWidgets.QRadioButton('509')
+        self.radioButton2.setGeometry(QtCore.QRect(130, 10, 100, 20))
+        self.vbox_car.addWidget(self.radioButton2)
 
-        self.radioButton = QtWidgets.QRadioButton('992')
-        self.radioButton.setGeometry(QtCore.QRect(130, 10, 100, 20))
-        self.vbox_car.addWidget(self.radioButton)
+        self.radioButton3 = QtWidgets.QRadioButton('992')
+        self.radioButton3.setGeometry(QtCore.QRect(130, 10, 100, 20))
+        self.vbox_car.addWidget(self.radioButton3)
+
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
