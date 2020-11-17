ed0144f678a17cc2108bcfb35a7d6e21213c43d9
akira <linkomizin@gmail.com>
2020-11-17 19:15:53 +0300

    Update qt5.py
    общая функциональность выбора элементов на экре и запись полученнго в файл

--- qt5.py
+++ qt5.py
@@ -2,9 +2,12 @@ import sys
 from PyQt5 import QtCore, QtWidgets
 from datetime import datetime
 
-data_now = datetime.now().strftime('%Y, %m, %d')
-
-
+val = { 'date' : '','car_name': '',
+	 'car_number':'', 'km_a': '150000', 'km_b': '150324',
+	 'fuel': '45,67', 'km': '29'}
+cars = {'К400ХН76': 'CHEVROLET NIVA',
+        'А509КО76': 'Toyota RAV4',
+        'В992РО76': 'UAZ Pickup'}
 
 class Ui_Form(QtWidgets.QWidget):
     def __init__(self, parent=None):
@@ -31,10 +34,15 @@ class Ui_Form(QtWidgets.QWidget):
         self.dateEdit.setDate(QtCore.QDate.currentDate())
         self.dateEdit.setCalendarPopup(True)
         self.vbox.addWidget(self.dateEdit)
+        a = self.dateEdit.date()
+        a = a.toPyDate()
+        self.dateEdit.dateChanged.connect(self.date_add)
+
 
         self.pushButton = QtWidgets.QPushButton("Save .docx")
         self.pushButton.setGeometry(QtCore.QRect(0, 60, 113, 32))
         self.vbox.addWidget(self.pushButton)
+        self.pushButton.clicked.connect(self.f_write)
 
         self.radioButton = QtWidgets.QRadioButton('400')
         self.radioButton.setGeometry(QtCore.QRect(130, 10, 100, 20))
@@ -57,11 +65,23 @@ class Ui_Form(QtWidgets.QWidget):
         self.label = QtWidgets.QLabel()
         self.vbox_text.addWidget(self.label)
 
+    def date_add(self, a):
+        date = a.toString('dd.MM.yyyy')
+        self.label.setText(date)
+        val['date'] = date
 
     def on_clicked(self):
         radioButton = self.sender()
         if radioButton.isChecked():
             self.label.setText(radioButton.value)
+            val['car_number'] = radioButton.value
+            val['car_name'] = cars[radioButton.value]
+
+    def f_write(self):
+        a = ('tmp/tmp.txt')
+        with open(a, 'w') as f:
+            f.write(str(val))
+
 
 
 if __name__ == '__main__':
